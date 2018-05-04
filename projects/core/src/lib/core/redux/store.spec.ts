9aefdf09336617be9d5a8b294948e287f9beabd3
Murat Akkas <ma@macoding.de>
2018-05-04 21:55:23 +0200

    update to angular 6 and restructure project

--- projects/core/src/lib/core/redux/store.spec.ts
+++ projects/core/src/lib/core/redux/store.spec.ts
@@ -10,26 +10,17 @@ describe('Store', () => {
     let store: Store<ITestState>;
 
     function createStore(): void {
-        store = new Store<ITestState>();
-    }
-
-    function initStore(): void {
-        store.initStore({
-            count: 1,
-            title: 'test',
-            value: 1234
-        });
+        store = new Store<ITestState>({
+          count: 1,
+          title: 'test',
+          value: 1234
+      });
     }
 
     beforeEach(() => {
         createStore();
     });
-    it('store is empty', () => {
-        expect(store).toBeTruthy();
-        expect(store.getState()).toBe({});
-    });
     it(`store is initialized`, () => {
-        initStore();
         expect(store).toBeTruthy();
         expect(store.getState()).toEqual({
             count: 1,
